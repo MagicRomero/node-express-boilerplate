72c1cde5561f74ec3a5beb0e053d7a42f980f9d8
MagicRomero <magicromero@protonmail.com>
2020-10-12 08:24:45 +0100

    New options for mongo databsae connections

--- src/loaders/database_loader.ts
+++ src/loaders/database_loader.ts
@@ -7,6 +7,9 @@ export default async (): Promise<any> => {
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 500,
+    connectTimeoutMS: 10000,
   });
 
   console.log(`MongoDB Connected: ${connection.connection.host}`);
